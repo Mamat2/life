9c6949837cc67becd34117068b77a54ca3b9df5a
mamat <matt.sparrow.ru@gmail.com>
2019-01-05 21:06:06 +0700

    добавлена симуляция игры и кнопка очистки

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-let field ;
+let field , intervalId=-1 ;
 
 init();
 function init() {
 	//alert('run');
 	$('[name = "columns"]').on("change", evt=> {
 		create();
-	})
+	});
 	$('[name = "rows"]').on("change", evt=> {
 		create();
-	})
-
+	});
+	$('#step').click(evt=>{ 
+		newState();
+		update();
+	});
+	$ ('#run').click(evt=>{
+		if (intervalId == -1){
+			intervalId=setInterval(()=>{newState(); update()}, 1000);
+		} else {
+			clearInterval(intervalId);
+			intervalId=-1;
+		}
+		
+	});
+	$ ('#clear').click(evt=>{
+		
+		for(let r=0; r<field.length ; r++){
+			for (let c=0 ; c<field[r].length;c++){
+				field[r][c] = false;
+			}
+		}
+		update();
+	});
 	
 	create();
 }
@@ -19,7 +40,6 @@ function create() {
 		rows = $('[name = "rows"]').val();
 	field = [];
 	$("#field").empty();
-	console.log(cols,rows);
 	for(var d=0 ; d< rows ; d++ ){
 		let tr = $("<tr/>");
 		let row =[];
@@ -30,28 +50,81 @@ function create() {
 		$("#field").append(tr);
 		field.push(row);
 	}
-	console.log(field);
 	$("#field td").on("click", evt=>{
+		console.log(evt.target);
 		let col=evt.target.parentElement.rowIndex;
 		let row = evt.target.cellIndex ;
+		console.log(col, row);
 		field[col][row]= !field[col][row];
 		update();
 	});	
 }
 
 function update(){
-	console.log(field);
 	for (let r =0 ; r<field.length; r++){
 		let tr = $("#field tr")[r];
 		for(let c=0; c<field[r].length; c++){
-			//console.log($(tr).children());
 			if (field[r][c]){
 				$($(tr).children()[c]).addClass("selected"); 
-
 			} else {
 				$($(tr).children()[c]).removeClass("selected"); 
 			}
 		}
-
 	}
 }
+
+
+
+function newState (){
+	const tmp=[];
+	for (let r=0 ; r<field.length ; r++){
+		tmp.push([]);
+		for (let c=0; c<field[r].length;c++){
+			let l= countNeigbors(r,c);
+			if (field[r][c]){
+				if ( l ==2 || l == 3){
+					tmp[r].push(true);
+				} else {
+					tmp[r].push(false);
+				}
+			} else {
+				if (l == 3){
+					tmp[r].push(true);
+				} else{
+					tmp[r].push(false);
+				}
+			}
+		}
+	}
+	field = tmp.slice(0);
+	
+	function countNeigbors(r, c){
+		let l = 0 ;
+		for (let j=r-1; j<=r+1; j++){
+			let j1=j;
+			if (j==-1){
+				j1=field.length -1;
+			} else if (j == field.length){
+				j1=0;
+			}
+
+			for (let i= c-1; i<=c+1 ; i++){
+				let i1= i;
+				if (i==-1){
+					i1=field[r].length -1;
+				} else if (i == field[r].length){
+					i1=0;
+				}
+
+				if(j1==r && i1==c){
+					continue;
+				}
+				if (field[j1][i1]){
+					l+=1;
+				}
+			}
+		}
+		return l;
+	}
+}
+
